b572f6a315c946b9988102e288fd5689431a38d5
smdr <seelammuralidharreddy.com>
2024-11-04 10:09:56 -0500

    messages if git

---
+++ src/main/java/naresh/it/NareshDevelopProject.java
@@ -0,0 +1,12 @@
+package naresh.it;
+
+import java.util.Base64;
+
+public class NareshDevelopProject {
+    public String test(String s){
+        return Base64.getEncoder().encodeToString(s.getBytes());
+    }
+    public String test2(String s){
+        return  Base64.getDecoder().decode(s).toString();
+    }
+}
